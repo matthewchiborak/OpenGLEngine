40956e811820c9f1409d61bd6abe718888f70820
matthewchiborak <matthewchiborak@hotmail.com>
2017-05-31 17:23:18 -0400

    Dont want to accidetnly break something

--- source/TestScene.cpp
+++ source/TestScene.cpp
@@ -14,14 +14,16 @@ void TestScene::init()
 	//addGameObjectToScene(new GameObject("Ship", ".\\res\\arwing.obj", TextureManager::getTextureManager()->getTexture("Arwing"), ShaderManager::getShaderManager()->getShader("Phong")));
 	//Mesh* testMesh = new Mesh(".\\res\\arwing.obj");
 	
-	MeshRenderer* testMR = new MeshRenderer(MeshManager::getMeshManager()->getMesh("Arwing"), TextureManager::getTextureManager()->getTexture("Arwing"), ShaderManager::getShaderManager()->getShader("Phong"));
+	//MeshRenderer* testMR = new MeshRenderer(MeshManager::getMeshManager()->getMesh("Arwing"), TextureManager::getTextureManager()->getTexture("Arwing"), ShaderManager::getShaderManager()->getShader("Phong"));
 	//root.addComponent(testMR);
+	MeshRenderer* testMR = new MeshRenderer(MeshManager::getMeshManager()->getMesh("Arwing"), TextureManager::getTextureManager()->getTexture("Arwing"));
 	GameObject* testObject = new GameObject();
 	testObject->addComponent(testMR);
 	root.addChild(testObject);
 
 	GameObject* testObject2 = new GameObject();
-	MeshRenderer* testMR2 = new MeshRenderer(MeshManager::getMeshManager()->getMesh("Arwing"), TextureManager::getTextureManager()->getTexture("Arwing"), ShaderManager::getShaderManager()->getShader("Phong"));
+	//MeshRenderer* testMR2 = new MeshRenderer(MeshManager::getMeshManager()->getMesh("Arwing"), TextureManager::getTextureManager()->getTexture("Arwing"), ShaderManager::getShaderManager()->getShader("Phong"));
+	MeshRenderer* testMR2 = new MeshRenderer(MeshManager::getMeshManager()->getMesh("Arwing"), TextureManager::getTextureManager()->getTexture("Arwing"));
 	testObject2->addComponent(testMR2);
 	testObject->addChild(testObject2);
 	testObject2->move(Vec9::createVec9(1, 0, 0, 0, 0, 0, 0, 0, 0));
